a637f97442a53ef8a0c84e2074f5875fa87a07a2
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-07-15 09:13:44 -0500

    Update 1-export_to_CSV.py
    PEP8 corrections to line length

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" Python script to export data in the CSV format"""
+"""Python script to export data in the CSV format"""
 
 import requests
 import sys
 import csv
 
-
 def fetch_employee_todo_progress(employee_id):
     """
-    Fetches the TODO list progress for a given employee ID from JSONPlaceholder API
-    and exports it into a CSV file named USER_ID.csv.
+    Fetches TODO list progress for given employee ID from JSON API
+    exports into a CSV file named USER_ID.csv.
 
     Args:
-    - employee_id (int): The ID of the employee whose TODO list progress is to be fetched and exported.
+    - employee_id (int): ID of employee whose TODO list progress fetched and exported.
     """
     base_url = 'https://jsonplaceholder.typicode.com'
     todos_url = f'{base_url}/todos?userId={employee_id}'
@@ -23,7 +22,7 @@ def fetch_employee_todo_progress(employee_id):
         response = requests.get(user_url)
         response.raise_for_status()
         user_data = response.json()
-        employee_name = user_data['username']  # Use 'username' instead of 'name' as per JSONPlaceholder API
+        employee_name = user_data['username']
 
         # Fetching todo list for the employee
         response = requests.get(todos_url)
@@ -36,17 +35,22 @@ def fetch_employee_todo_progress(employee_id):
         # Writing to CSV file
         with open(csv_filename, mode='w', newline='', encoding='utf-8') as csv_file:
             csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+            csv_writer.writerow([
+                'USER_ID', 'USERNAME',
+                'TASK_COMPLETED_STATUS', 'TASK_TITLE'
+            ])
 
             for task in todos_data:
                 task_completed = "Completed" if task['completed'] else "Not Completed"
-                csv_writer.writerow([employee_id, employee_name, task_completed, task['title']])
+                csv_writer.writerow([
+                    employee_id, employee_name,
+                    task_completed, task['title']
+                ])
 
         print(f"Exported TODO list for Employee ID {employee_id} to {csv_filename}")
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
-
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
