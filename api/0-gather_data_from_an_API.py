f58bb5b01a9bd4d3a6a6572781614d655df99980
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-07-15 11:29:55 -0500

    Update 0-gather_data_from_an_API.py
    alphabetized libraries

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,32 +5,37 @@ using JSONPlaceholder API.
 """
 
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-
-    import requests
-    from sys import argv
     if len(argv) < 2:
-        exit()
-    todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
-        .format(argv[1]))
-    name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={}"
-        .format(argv[1]))
-    name = name.json()
-    name = name[0]["name"]
-    todo = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
-    todo = todo.json()
-    todo = len(todo)
-    todos = todos.json()
-    todo_list = []
+        print("Usage: python script.py <employee_id>")
+        exit(1)
 
-    for x in todos:
-        todo_list.append("\t {}".format(x["title"]))
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, len(todos), todo))
-    for y in todo_list:
-        print(y)
+    employee_id = argv[1]
+    
+    # Fetch employee information
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    user_data = user_response.json()
+    
+    if not user_data:
+        print(f"Employee with ID {employee_id} not found.")
+        exit(1)
+    
+    employee_name = user_data['name']
+
+    # Fetch completed todos for the employee
+    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos",
+                                  params={"userId": employee_id, "completed": "true"})
+    todos_data = todos_response.json()
+    total_tasks = len(todos_data)
+
+    # Prepare and print task list
+    task_list = []
+    for todo in todos_data:
+        task_list.append(f"\t{todo['title']}")
+
+    # Print output
+    print(f"Employee {employee_name} is done with tasks ({total_tasks}/{total_tasks}):")
+    for task in task_list:
+        print(task)
