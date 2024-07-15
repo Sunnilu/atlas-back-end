d85b2f420e602b0aaeb7070997802932bf93d4bd
Suntha <Suntha.Lucas@atlasschool.com>
2024-07-15 20:26:24 +0000

    removed extra lines

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,12 +13,13 @@ def fetch_employee_todo_progress(employee_id):
     Fetches and displays the TODO list progress for a given employee ID.
 
     Args:
-    - employee_id (str): The ID of the employee whose TODO list progress is to be fetched.
+    - employee_id (str): The ID of the employee whose
+     TODO list progress is to be fetched.
 
     Prints:
     - Displays the employee's TODO list progress in the specified format.
     """
-    # Fetch employee name
+    
     name_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
     name_data = name_response.json()
 
@@ -28,14 +29,14 @@ def fetch_employee_todo_progress(employee_id):
 
     employee_name = name_data['name']
 
-    # Fetch all todos for the employee
+    
     todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
     todos_data = todos_response.json()
 
     # Filter completed todos
     completed_todos = [todo for todo in todos_data if todo['completed']]
 
-    # Prepare and print task list
+    # Prepare 
     print(f"Employee {employee_name} is done with tasks ({len(completed_todos)}/{len(todos_data)}):")
     for todo in completed_todos:
         print(f"\t{todo['title']}")
@@ -48,5 +49,3 @@ if __name__ == "__main__":
 
     employee_id = sys.argv[1]
     fetch_employee_todo_progress(employee_id)
-
-
