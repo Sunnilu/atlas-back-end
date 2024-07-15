4087194a216da6e87e96f341b1f35d1ace311dc3
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-07-15 11:17:15 -0500

    Update 0-gather_data_from_an_API.py
    line length corrections made

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,32 +8,29 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com/"
+    import requests
+    from sys import argv
+    if len(argv) < 2:
+        exit()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(argv[1]))
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}"
+        .format(argv[1]))
+    name = name.json()
+    name = name[0]["name"]
+    todo = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+    todo = todo.json()
+    todo = len(todo)
+    todos = todos.json()
+    todo_list = []
 
-    try:
-        # Fetch user information
-        user_response = requests.get(f"{base_url}users/{employee_id}")
-        user = user_response.json()
-        employee_name = user['name']
-
-        # Fetch TODO list for the employee
-        todos_response = requests.get(f"{base_url}todos", params={"userId": employee_id})
-        todos = todos_response.json()
-
-        # Filter completed tasks
-        completed_tasks = [todo['title'] for todo in todos if todo['completed']]
-
-        # Print TODO list progress
-        print(f"Employee {employee_name} is done with tasks "
-              f"({len(completed_tasks)}/{len(todos)}):")
-
-        for task in completed_tasks:
-            print(f"\t{task}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    for x in todos:
+        todo_list.append("\t {}".format(x["title"]))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(todos), todo))
+    for y in todo_list:
+        print(y)
